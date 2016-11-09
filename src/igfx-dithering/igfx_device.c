dc520423251b07e6199c922ada045feb9257a9ab
nitrocaster <paul-kv@yandex.ru>
2016-11-10 00:52:56 +0300

    Move register I/O to igfx_device.

--- src/igfx-dithering/igfx_device.c
+++ src/igfx-dithering/igfx_device.c
@@ -1,5 +1,11 @@
 ﻿#include "igfx_device.h"
 #include "WinRing0.h"
+#include "InpOut.h"
+
+static HANDLE mapped_mem_handle = NULL;
+static uint8_t *mapped_phys_addr = NULL;
+static uint8_t *mapped_lin_addr = NULL;
+static uint32_t mapped_size = 0;
 
 int igfx_device_find(uint32_t *address, uint16_t *ven, uint16_t *dev)
 {
@@ -31,3 +37,48 @@ int igfx_device_get_iobase64(uint32_t address, uint32_t bar0, uint32_t bar1, uin
         return 1;
     return 0;
 }
+
+static uint8_t *reg_memmap(uint8_t *address, uint32_t size)
+{
+    uint8_t *lin_addr;
+    if (address==mapped_phys_addr && size==mapped_size)
+        lin_addr = mapped_lin_addr;
+    else
+    {
+        if (mapped_mem_handle)
+            UnmapPhysicalMemory(mapped_mem_handle, mapped_lin_addr);
+        mapped_phys_addr = address;
+        mapped_size = size;
+        mapped_mem_handle = NULL;
+        mapped_lin_addr = NULL;
+        if (!size)
+            return NULL;
+        lin_addr = MapPhysToLin(address, size, &mapped_mem_handle);
+        mapped_lin_addr = lin_addr;
+    }
+    return lin_addr;
+}
+
+int igfx_device_read(uint8_t *address, uint32_t size, uint8_t *data)
+{
+    uint8_t *lin_addr = reg_memmap(address, size);
+    if (lin_addr)
+    {
+        memcpy(data, lin_addr, size);
+        return 0;
+    }
+    return 1;
+}
+
+int igfx_device_write(uint8_t *address, uint32_t size, uint8_t *data)
+{
+    uint8_t *lin_addr = reg_memmap(address, size);
+    if (lin_addr)
+    {
+        memcpy(lin_addr, data, size);
+        return 0;
+    }
+    return 1;
+}
+
+void igfx_device_unmap(void) { reg_memmap(NULL, 0); }
--- src/igfx-dithering/igfx_device.h
+++ src/igfx-dithering/igfx_device.h
@@ -5,3 +5,6 @@
 int igfx_device_find(uint32_t *address, uint16_t *ven, uint16_t *dev);
 int igfx_device_get_iobase32(uint32_t address, uint32_t bar0, uint32_t bar1, uint32_t *iobase);
 int igfx_device_get_iobase64(uint32_t address, uint32_t bar0, uint32_t bar1, uint64_t *iobase);
+int igfx_device_read(uint8_t *address, uint32_t size, uint8_t *data);
+int igfx_device_write(uint8_t *address, uint32_t size, uint8_t *data);
+void igfx_device_unmap(void);
