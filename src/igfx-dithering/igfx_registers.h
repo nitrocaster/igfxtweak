c901f08a6643cf0025ba6862d8670a43d49cf434
nitrocaster <paul-kv@yandex.ru>
2016-11-09 22:57:27 +0300

    Implement register I/O.

---
+++ src/igfx-dithering/igfx_registers.h
@@ -0,0 +1,7 @@
+﻿#pragma once
+#include "config.h"
+#include <stdint.h>
+
+int igfx_register_read(uint8_t *address, uint32_t size, uint8_t *data);
+int igfx_register_write(uint8_t *address, uint32_t size, uint8_t *data);
+void igfx_register_unmap(void);
